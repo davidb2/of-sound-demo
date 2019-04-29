d739c400fdd8a5046276e7d3594801f98ca51fb5
= <brewcrew5498@outlook.com>
2019-04-28 20:49:40 -0500

    Example multiplay.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,71 +1,98 @@
 #include "ofApp.h"
 
+const string kDir = "/home/david/playground/spring-2019/cs126/of-sound-demo";
+const string kPianoDir = "Piano-Sounds-High-Quality-/All 88 Key Sounds EDITED (mp3)";
+const double kThreshold = 0.05;
+
+void getNote(const Note& note, ofSoundPlayer* player) {
+  const string fileName = kDir + "/" + kPianoDir + "/" + note.toString(true) + ".mp3";
+  player->load(fileName);
+}
+
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
+  noteIdx_ = 0;
+
+  players_.resize(kNumNotes);
+
+  for (ofSoundPlayer& player : players_) {
+    player.setMultiPlay(true);
+  }
+
+  getNote(kBrokenThirdsUp[noteIdx_], &players_[0]);
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+  if (noteIdx_ == 0 && !players_[0].isPlaying()) {
+    players_[0].play();
+  }
+
+  if (noteIdx_ < kNumNotes - 1 && players_[noteIdx_].getPosition() > kThreshold) {
+    noteIdx_++;
+    getNote(kBrokenThirdsUp[noteIdx_], &players_[noteIdx_]);
+    players_[noteIdx_].play();
+  }
+
+  ofSoundUpdate();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::keyPressed(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::keyReleased(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::mouseMoved(int x, int y ) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::mouseDragged(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::mousePressed(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::mouseReleased(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::mouseEntered(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::mouseExited(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
+void ofApp::windowResized(int w, int h) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
+void ofApp::gotMessage(ofMessage msg) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo) {
 
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -2,23 +2,80 @@
 
 #include "ofMain.h"
 
-class ofApp : public ofBaseApp{
+struct Note {
+  const string name;
+  const int octave;
+  const double length;
 
-	public:
-		void setup();
-		void update();
-		void draw();
+  Note(const string& name, const int octave, const double length) :
+    name(name), octave(octave), length(length) { }
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+  string toString(const bool edited) const {
+    stringstream ss;
+    if (!edited) {
+      ss << name << octave;
+      return ss.str();
+    }
+
+    int number = 0;
+    if (name == "A") number = 0;
+    if (name == "Bb") number = 1;
+    if (name == "B") number = 2;
+    if (name == "C") number = 3;
+    if (name == "Db") number = 4;
+    if (name == "D") number = 5;
+    if (name == "Eb") number = 6;
+    if (name == "E") number = 7;
+    if (name == "F") number = 8;
+    if (name == "Gb") number = 9;
+    if (name == "G") number = 10;
+    if (name == "Ab") number = 11;
+
+    if (name == "A" || name == "Bb" || name == "B") {
+      number += 13 * (octave + 1);
+    } else {
+      number += 13 * octave;
+    }
+    number -= (number + 1) / 13;
+    number++;
+    ss << number;
+    return ss.str();
+  }
+
+};
+
+constexpr int kO = 4;
+constexpr int kNumNotes = 15;
+const Note kBrokenThirdsUp[kNumNotes] = {
+  {"C", kO, 1}, {"E", kO, 1},
+  {"D", kO, 1}, {"F", kO, 1},
+  {"E", kO, 1}, {"G", kO, 1},
+  {"F", kO, 1}, {"A", kO, 1},
+  {"G", kO, 1}, {"B", kO, 1},
+  {"A", kO, 1}, {"C", kO+1, 1},
+  {"B", kO, 1}, {"D", kO+1, 1},
+  {"C", kO+1, 1}
+};
+
+class ofApp : public ofBaseApp {
+ public:
+  void setup();
+  void update();
+  void draw();
+
+  void keyPressed(int key);
+  void keyReleased(int key);
+  void mouseMoved(int x, int y );
+  void mouseDragged(int x, int y, int button);
+  void mousePressed(int x, int y, int button);
+  void mouseReleased(int x, int y, int button);
+  void mouseEntered(int x, int y);
+  void mouseExited(int x, int y);
+  void windowResized(int w, int h);
+  void dragEvent(ofDragInfo dragInfo);
+  void gotMessage(ofMessage msg);
+
+ private:
+  vector<ofSoundPlayer> players_;
+  int noteIdx_;
 };
